8f1efdc6160022087b55de2a15d1c93e0f77478c
Jinjing <jinjing93@gmail.com>
2025-10-06 10:54:49 +1100

    exercise 1 first part

---
+++ exercise 1.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(tidyverse)
+library(GEOquery)
+
+data <- read.csv(file = "C:/Users/Charlotte/Downloads/GSE183947_fpkm.csv")
+
+# get meta data
+gse <- getGEO(GEO = 'GSE183947', GSEMatrix = TRUE)
+
+gse
+
+metadata <- pData(phenoData(gse[[1]]))
+#head(metadata)
+
+
+# metadata.subset <- select(metadata, c(1, 10, 11, 17))
+metadata.modified <- metadata %>%
+  select(1,10,11,17) %>%
+  rename(tissue = characteristics_ch1)%>%       # rename columns
+  rename(metastasis = characteristics_ch1.1)%>%
+  mutate(tissue = gsub("tissue: ","", tissue))%>% #remove prefix "tissue"
+  mutate(tissue = gsub("metastasis: ","", metastasis))
+
+# reshaping data
+data %>%
+  rename(gene = X)%>%
+  gather(key = 'samples', value = 'FPKM', -gene)
